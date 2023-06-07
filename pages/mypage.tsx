fadd0d81ff3dc3e97ac1cb6eed0a97c1f159f7f1
Takuzen Toh <ttoh@takuzens-mini.lan>
2023-06-07 21:40:09 +0900

    feat: add algolia search to mypage (no content yet)

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,32 +1,39 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useEffect } from 'react';
+import { autocomplete } from '@algolia/autocomplete-js';
+import '@algolia/autocomplete-theme-classic';
 
 export default function Mypage() {
+  useEffect(() => {
+    autocomplete({
+      container: '#autocomplete',
+      placeholder: 'Search for products',
+      getSources() {
+        return [];
+      },
+    });
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-centet justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <div className="relative flex place-items-center text-3xl">
+        <div id="autocomplete"></div>
+
         <Image
-          src="/searchbar_sample.png"
-          alt="Fuse Logo"
-          width={360}
-          height={40}
-          priority
-        />
-      </div>
-      <Image
           src="/roam_image.png"
           alt="Roam Image"
           width={450}
           height={300}
           priority
         />
-      <div className="relative flex place-items-center text-3xl">
-        <p>
-          <Link href="/notes">
-            最近のノート
-          </Link>
-        </p>
+
+        <div className="relative flex place-items-center text-3xl">
+          <p>
+            <Link href="/notes">
+              最近のノート
+            </Link>
+          </p>
         </div>
       </div>
     </main>
