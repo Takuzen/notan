779d558eb8bfd69beacc44d93cbd4b322c171648
Takuzen Toh <ttoh@takuzens-mini.lan>
2023-06-16 18:13:35 +0900

    feat: add new sample note images and links

--- src/app/mypage_trial/page.tsx
+++ src/app/mypage_trial/page.tsx
@@ -26,7 +26,7 @@ export default function Mypage() {
   useEffect(() => {
     autocomplete({
       container: '#autocomplete',
-      placeholder: 'フルテキスト検索（まもなく、登場）',
+      placeholder: 'GPT検索（まもなく、登場）',
       getSources() {
         return [];
       },
@@ -41,7 +41,7 @@ export default function Mypage() {
           <div className='flex flex-wrap justify-between gap-x-5 gap-y-8'>
             <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
             <Image
-                className='object-fill'
+                className='object-fit'
                 src="/note_sample_0.png"
                 alt="Fuse Logo"
                 width={100}
@@ -49,142 +49,246 @@ export default function Mypage() {
                 priority
               />
               </Link>
-            <Image
-                className='object-fill'
-                src="/note_sample_1.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/note_sample_2.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/scan_sample_0.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/scan_sample_1.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/note_sample_3.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/scan_sample_2.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/note_sample_4.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/note_sample_5.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/scan_sample_3.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/scan_sample_4.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/scan_sample_5.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/scan_sample_6.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/scan_sample_7.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/note_sample_6.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/note_sample_7.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/note_sample_8.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
-            <Image
-              className='object-fill'
-                src="/note_sample_9.png"
-                alt="Fuse Logo"
-                width={100}
-                height={100}
-                priority
-              />
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                  className='object-fit'
+                  src="/note_sample_1.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_2.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/scan_sample_0.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/scan_sample_1.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_3.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/scan_sample_2.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_4.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_5.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/scan_sample_3.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/scan_sample_4.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/scan_sample_5.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/scan_sample_6.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/scan_sample_7.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_6.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_7.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_8.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_10.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_11.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_12.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_13.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_14.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_15.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_16.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
+            <Link href="https://issuu.com/takuzen/docs/notebook_0_p10">
+              <Image
+                className='object-fit'
+                  src="/note_sample_17.png"
+                  alt="Fuse Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+            </Link>
           </div>
           {/* 最後の行だけ左詰にしたい */}       
         </div>
